9694d33e9d8a6cba53bb3db2f63872ad94608764
rishikeshydv <rishikeshadh4@gmail.com>
2024-01-17 15:44:08 +0545

    dream http added

--- backend/ocaml/ocamServer/bin/main.ml
+++ backend/ocaml/ocamServer/bin/main.ml
@@ -1,3 +1,6 @@
+
+//defining types
+
 type balances = (string * int) list;;
 
 type user = {
@@ -12,8 +15,23 @@ type order = {
 };;
 
 
+//user retrieval
 
 let users: user list = [
   { id: "1";balances: [("GOOGLE": 10),"USD": 50000]};
   { id: "2";balances: ["GOOGLE": 10,"USD": 50000]};
-  ];
+];;
+
+
+//defining bids and asks
+let bids list = [];;
+let asks list = [];;
+
+
+let () =
+  Dream.run
+  @@ Dream.router [
+    Dream.get "/echo/:word" @@ fun request ->
+      Dream.html (Dream.param "word" request);
+  ]
+
