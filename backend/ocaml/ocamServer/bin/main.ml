4208ffd1fd25a9c7475d962bc08e0d8ba38a7d02
rishikeshydv <rishikeshadh4@gmail.com>
2024-01-18 23:43:33 +0545

    flip prices func

--- backend/ocaml/ocamServer/bin/main.ml
+++ backend/ocaml/ocamServer/bin/main.ml
@@ -27,11 +27,35 @@ let users: user list = [
 let bids list = [];;
 let asks list = [];;
 
+let TICKER = "GOOGLE";;
+
+//defining a balance flipping function that flips stock and money of buyer and seller
+
+let fB = fun flipBalance userId1: string userId2: string quantity: number price: number ->
+let user1 = List.find(fun u -> u.id=userId1) users in
+let user2 = List.find(fun u -> u.id = userId2.id) users in
+
+//here we will be defining an action to be taken when users are not found
+let () : unit = if not user1 && not user2 then
+  () ;;
+
+user1.balances[TICKER] += quantity;;
+user2.balances[TICKER] -= quantity;;
+user1.balances['USD'] += quantity*price;;
+user2.balances['USD'] -= quantity*price;;
+
+;;
+
+
+
+
 
 let () =
   Dream.run
   @@ Dream.router [
-    Dream.get "/echo/:word" @@ fun request ->
+    Dream.post "/order" @@ fun request ->
       Dream.html (Dream.param "word" request);
   ]
+  
+
 
