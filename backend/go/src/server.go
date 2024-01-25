c6a18dd90b86682f2ccbd024f52a6b23c587c65c
rishikeshydv <rishikeshadh4@gmail.com>
2024-01-25 20:12:56 +0545

    chore: user update func added

--- backend/go/src/server.go
+++ backend/go/src/server.go
@@ -1,7 +1,5 @@
 package main
 
-import "fmt"
-
 type Balances map[string]int
 
 type User struct {
@@ -17,6 +15,46 @@ type Order struct {
 
 var Symbol = "GOOGLE"
 
-func main(
-	fmt.Println("Hello")
-)
+func main() {
+	bids := []Order{}
+	asks := []Order{}
+
+	users := []User{
+		{id: "1",
+			balances: Balances{
+				"GOOGLE": 10,
+				"USD":    500,
+			},
+		},
+		{
+			id: "2",
+			balances: Balances{
+				"GOOGLE": 20,
+				"USD":    1000,
+			},
+		},
+	}
+}
+
+func UserUpdate(user1Id string, user2Id string, price int, quantity int, users []User) {
+	var firstUser *User
+	var secondUser *User
+
+	for _, user := range users {
+		if user.id == user1Id {
+			firstUser = &user
+			break
+		} else if user.id == user2Id {
+			secondUser = &user
+		}
+	}
+
+	firstUser.balances[Symbol] -= quantity
+	secondUser.balances[Symbol] += quantity
+	firstUser.balances["USD"] += quantity * price
+	secondUser.balances["USD"] -= quantity * price
+}
+
+func FillOrders() {
+
+}
